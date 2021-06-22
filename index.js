8dffcce28acc4770c45729ef599c97ed76aed47f
Arto <artavazd_arto@yahoo.com>
2021-06-22 23:37:51 +0900

    final version

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const fetch = require("node-fetch");
 
 app.listen(PORT, () => console.log(`Alive on http://localhost:${PORT}`))  
 
-async function getRequestedFlagData() {
-    const location  = "Armenia";
-    const url = "https://restcountries.eu/rest/v2/name/";
-
-    const data = await fetch(url+location)
+async function getRequestedFlagData(location) {
+    const url = `https://restcountries.eu/rest/v2/name/${location}`;
+    const data = await fetch(url)
                 .then(response => response.json())
                 .then(data => data[0].flag);
     return data;                     
 };
 
 
-app.get('/', async (req, res) => {
+app.get('/:location', async (req, res) => {
  try {
-        let results = await getRequestedFlagData();
+        let results = await getRequestedFlagData(req.params.location);
         res.status(200).send(results);
     } catch (error) {
         res.json({message: error.message});
